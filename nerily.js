02b4823ce1bef032b28c53135c4c04973615a467
nubela <nubela@ctrleff.com>
2013-02-05 02:10:45 +0800

    updated extension to have a few more options

--- nerily.js
+++ nerily.js
@@ -1,21 +1,44 @@
+var HELP_URL = "http://spawt.me";
+
 function showNotification(title, body) {
-    webkitNotifications.createNotification(
-        '48.png', // icon url - can be relative
-        title, // notification title
-        body  // notification body text
-    ).show();
+    var notification = window.webkitNotifications.createNotification('icon.png', title, body);
+
+    notification.ondisplay = function(event) {
+        setTimeout(function() {
+            event.currentTarget.cancel();
+        }, 4000);
+    };
+
+    notification.onclick = function() {
+        this.cancel();
+    };
+
+    notification.show();
 }
 
+function beginLabeling(tab, lbl, selectedText) {
+    chrome.tabs.executeScript(tab.id, {file:"contentscript.js"}, function () {
+        chrome.tabs.sendRequest(tab.id, {method:"getText"}, function (response) {
+            if (response.method == "getText") {
+                allText = response.data;
+                label(lbl, selectedText, allText)
+                showNotification("Successfully labelled.","");
+            }
+        });
+    });
+}
 function onClickHandler(info, tab) {
     //new label
     if (info.menuItemId == "newLabel") {
         var lbl = window.prompt("What is the name of this label? (Example: ADDR)");
+        var selectedText = info.selectionText;
+
+        //check if label exists
         chrome.storage.local.get("labels", function (r) {
             var labels = r["labels"];
             if (labels == null) {
                 labels = [];
             }
-
             if (!labels.indexOf(lbl) > -1) {
                 labels.push(lbl);
                 chrome.storage.local.set({"labels":labels}, function () {
@@ -27,20 +50,29 @@ function onClickHandler(info, tab) {
                 });
             }
         });
+
+
+        //do labeling
+        beginLabeling(tab, lbl, selectedText);
     }
 
     //labeling clicks
     else if (info.menuItemId.substr(0, 3) == "lbl") {
         var selectedText = info.selectionText;
         var lbl = info.menuItemId.substr(3);
-        chrome.tabs.executeScript(tab.id, {file:"contentscript.js"}, function () {
-            chrome.tabs.sendRequest(tab.id, {method:"getText"}, function (response) {
-                if (response.method == "getText") {
-                    allText = response.data;
-                    label(lbl, selectedText, allText)
-                }
-            });
-        });
+        beginLabeling(tab, lbl, selectedText);
+    }
+
+    else if (info.menuItemId == "download") {
+        downloadTsv();
+    }
+
+    else if (info.menuItemId == "reset") {
+        reset();
+    }
+
+    else if (info.menuItemId == "help") {
+        openHelp();
     }
 };
 
@@ -89,6 +121,10 @@ function downloadTsv() {
         var url = "data:text/tab-separated-values;charset=utf-8," + encodeURIComponent(tsvStr);
         chrome.tabs.create({url:url});
     });
+}
+
+function openHelp(){
+    chrome.tabs.create({url:HELP_URL});
 }
 
 /*
@@ -193,14 +229,42 @@ function dumpObjectIndented(obj, indent) {
     return result.replace(/,\n$/, "");
 }
 
+function reset() {
+    chrome.storage.local.clear();
+    showNotification("Done.","You have just cleared the training model dataset.");
+}
+
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 // Set up context menu tree at install time.
 chrome.runtime.onInstalled.addListener(function () {
 
-    //reset
-//    chrome.storage.local.set({"documents":[]}, function () {
-//    });
+    chrome.contextMenus.create({
+        "title":"NERILY Options",
+        "contexts":["all"],
+        "id":"options"
+    })
+
+    chrome.contextMenus.create({
+        "title":"Help",
+        "contexts":["all"],
+        "id":"help",
+        "parentId": "options"
+    })
+
+    chrome.contextMenus.create({
+        "title":"Reset",
+        "contexts":["all"],
+        "id":"reset",
+        "parentId": "options"
+    })
+
+    chrome.contextMenus.create({
+        "title":"Download Training Modelset",
+        "contexts":["all"],
+        "id":"download",
+        "parentId": "options"
+    })
 
     chrome.contextMenus.create({
         "title":"Label this as ..",
